53f9f81133205a16d1dd2d6158d0fa7fde1d95bb
mmtabish <80705398+mmtabish@users.noreply.github.com>
2024-09-07 11:49:00 +0000

    new code

---
+++ c++/switch/if_else.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int marks;
+    cin>>marks;
+
+    if(marks>33){
+        cout<<"you are passed ";
+    }else{
+        cout<<"you are fail";
+    }
+
+    return 0;
+}
