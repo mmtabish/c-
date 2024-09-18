669b5f0fbc9f43714ce11ebf3d15fee416c76f2e
mmtabish <80705398+mmtabish@users.noreply.github.com>
2024-09-18 14:20:09 +0530

    basic

--- test/redux.js
+++ test/redux.js
@@ -3,9 +3,52 @@ import {createStore} from 'redux';
 
 //reducer
 
-const reducer = (state ={count:0},  action)=> {
+const reducer1 = (state ={count:0},  action)=> {
 
     switch(action.type){
         case'INCREAMENT':
     }
 }
+
+
+
+const INCREAMENT ="redux/increment";
+const initialState ={
+    counter:0,
+};
+
+
+
+
+
+// store 
+
+import {createStore} from "redux";
+import {reducer} from "./reduxModule";
+
+const store = createStore(reducer);
+
+// get state
+
+const state = store.getState();
+
+
+//action & reducer
+
+// A reducer is a function that determines how the state of an application changes in response to an action. 
+// It’s a core concept in Redux, a state management library for JavaScript applications. Here’s a simple way to understand reducers:
+
+
+
+
+export const reducer =(state = initialState, action) => {
+    switch (action.type){
+        case INCREAMENT:
+            return {
+                counter: state.counter + action.amount
+            };
+            default:
+                return state;
+    }
+};
+export const increamemtAction(amount = 1,)
