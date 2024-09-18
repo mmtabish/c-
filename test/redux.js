a363286e0b88c4e4461f5d4e815277dc4fa58726
mmtabish <80705398+mmtabish@users.noreply.github.com>
2024-09-18 11:46:33 +0530

    redux_Lt

---
+++ test/redux.js
@@ -0,0 +1,11 @@
+import {useSelector,Provider,connect} from 'react-redux';
+import {createStore} from 'redux';
+
+//reducer
+
+const reducer = (state ={count:0},  action)=> {
+
+    switch(action.type){
+        case'INCREAMENT':
+    }
+}
