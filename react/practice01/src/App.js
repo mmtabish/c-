e2d25dbcc40596ff6c360ace34d6af1000a57be6
mmtabish <80705398+mmtabish@users.noreply.github.com>
2024-10-16 05:48:23 +0000

    normal change in app.js

--- react/practice01/src/App.js
+++ react/practice01/src/App.js
@@ -2,9 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const fruits = [
+    "apple",
+    "aopricot",
+    "banana",
+    "blueberry",
+    "cherry",
+    "canberry",
+    "Fig",
+    "Grape",
+  ];
+
   return (
     <div className="App">
-      
+      <input></input>
+      {fruits.map((fruits) => {
+        return <p>{fruits}</p>
+      } )}
     </div>
   );
 }
