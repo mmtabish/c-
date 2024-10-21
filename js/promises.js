b3aa43bb8cad5d2309987799d8dc4a87bec8d9b0
mmtabish <80705398+mmtabish@users.noreply.github.com>
2024-10-21 15:23:59 +0000

    c

--- js/promises.js
+++ js/promises.js
@@ -67,6 +67,21 @@ async function consumePromiseFive(){
 
 consumePromiseFive()
 
+async function getAllusers(){
+   try{
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    // console.log(response);
+    const data =await response.json()
+   console.log(data);
+   }catch(error){
+    console.log("E:", error);
+   }
+   
+
+}
+
+getAllusers()
+
 
 
 
