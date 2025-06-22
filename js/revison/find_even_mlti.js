167a2a6938d5ec06577a9fb6da8f0d16937bfc21
mmtabish <80705398+mmtabish@users.noreply.github.com>
2025-06-22 14:57:38 +0530

    change

--- js/revison/find_even_mlti.js
+++ js/revison/find_even_mlti.js
@@ -1,7 +1,21 @@
 
 let data = [22,66,55,25,65,76,95,26,95];
 let evenNum = [];
+let count = 0;
 
 for(i = 0; i<data.length; i++){
-    
+    if(data[i]%2===0){
+        // console.log(data[i]);
+        count++;
+        evenNum.push(data[i]*2)
+        // evenNum[i] *=2;
+    }
 }
+
+// for(i = 0;i<evenNum.length; i++){
+//     evenNum[i] *=2;
+//     // console.log(evenNum[i]);
+// }
+
+console.log(count);
+console.log(evenNum);
