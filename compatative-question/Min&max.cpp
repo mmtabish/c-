95a329b316d643b1e50d619b5d7930139059e35f
mmtabish <80705398+mmtabish@users.noreply.github.com>
2025-11-30 09:42:00 +0530

    mint work

--- compatative-question/Min&max.cpp
+++ compatative-question/Min&max.cpp
@@ -10,7 +10,7 @@ class Solution {
         //check data
         // pic//
         //just comment//llllklnl
-        //pllllll//kkln..l,llmiklglmkdfdb
+        //pllllll//kkln..l,llmiklglmkdfdbhkhkk
         //ppppllkkminimumllmbcvbvflskjslguy
         if(n == 0 ) return {-1 , -1 };
         
