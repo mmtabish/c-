29d4f031f21a4eae2a0016bcd49b5cfb891298fd
mmtabish <80705398+mmtabish@users.noreply.github.com>
2025-12-10 12:01:43 +0530

    d sv

--- compatative-question/Min&max.cpp
+++ compatative-question/Min&max.cpp
@@ -10,7 +10,7 @@ class Solution {
         //check data
         // pic//yiyiyhihihihjoijoj
         //just comment//llllklnlyihihi xc zhslk
-        //pllllll//kkln..l,llmiklglmkdfdbhkhkkmlkmlkvsndknmlvndlsdvffnsov
+        //pllllll//kkln..l,llmiklglmkdfdbhkhkkmlkmlkvsndknmlvndlsdvffnsovvxvds
         //ppppllkkminimumllmbcvbvflskjslguygogojohoh
         if(n == 0 ) return {-1 , -1 };
         
